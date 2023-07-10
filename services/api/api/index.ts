1c5364886d3bf6b68ec0dd33172d21ad8a2ce68a
hieudoanm <hieumdoan@gmail.com>
2023-07-10 10:46:04 +0700

    Redeploy API

--- services/api/api/index.ts
+++ services/api/api/index.ts
@@ -1,6 +1,23 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+import { json } from 'express';
 import http from 'http';
+import { createApolloServer } from '../src/apollo';
 import { app } from '../src/app';
 
+const startServer = async (apolloServer: ApolloServer) => {
+  await apolloServer.start();
+  app.use(
+    '/api/graphql',
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(apolloServer)
+  );
+};
+
 const httpServer: http.Server = http.createServer(app);
+const apolloServer: ApolloServer = createApolloServer(httpServer);
+startServer(apolloServer).catch((error) => console.error(error));
 
 export default httpServer;
